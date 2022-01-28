b9e91ee33927e425cf63c687d5629ba3698a2f9c
Joseph Loera <joeloera7@gmail.com>
2022-01-27 22:05:37 -0800

    String Variables Practice

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -4,47 +4,24 @@ namespace HolaWorld
 {
     internal class Program
     {
-        //Entry point of our program
+    
         static void Main(string[] args)
         {
-            //declaring a variable
-            int num1;
+            //string variable 
+            string myName = "Joseph";
+            
+            //string variable contatination
+            string message = "My name is " + myName;
 
-            //declaring multiple variables at once
-            int num3, num4, num5;
+            //message.ToUpper(); converts message to uppercase
+            string capsMessage = message.ToUpper();
 
-            //assigning a value to a variable
-            num1 = 13;
+            //message.ToLower(); converts the string message to lowercase
+            string lowerCaseMessage = message.ToLower();
 
-            //declaring and initializing a variable in one go
-            int num2 = 23;
-            num2 = 100;
-
-            //sum will be used to add the sum of the two integer variables
-            //int sum = num1 + num2;
-
-            //using concatination
-            Console.WriteLine("num1 is " + num1);
-            int sum = num1 + num2;
-            Console.WriteLine("num1 " + num1 + " + num2 " + num2 + " is " + sum);
-
-            double d1 = 3.1415;
-            double d2 = 5.1;
-            double dDiv = d1 / d2;
-            Console.WriteLine("d1/d2 is " + dDiv);
-
-            //adding at the end of the value specifies that we are working with a float
-            //Without the f the program will think we are trying to write a double
-            float f1 = 3.1415f;
-            float f2 = 5.1f;
-            float fDiv = f1 / f2;
-            Console.WriteLine("f1 / f2 is " + fDiv);
-
-            double dIDiv = d1 / num1;
-            Console.WriteLine("d1 / num1 is " + dIDiv);
+            
+            Console.WriteLine(lowerCaseMessage);
             Console.Read();
-
-
         }
     }
 }
