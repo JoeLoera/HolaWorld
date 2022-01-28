6d0d5aacef5be38d33ef46b7177f40726af3478d
Joseph Loera <joeloera7@gmail.com>
2022-01-28 06:10:32 -0800

    Making color changes to console and understanding namespace

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -1,26 +1,27 @@
-﻿using System;
+﻿//When a system is being used, it is highlighted. When it is not in use, it is greyed out.
+//Removing System will cause errors because it will not recognize console
+//We must include using System; namespace in order to use Console.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 
 namespace HolaWorld
 {
     internal class Program
     {
-    
+        //starting point of our program
         static void Main(string[] args)
         {
-            //string variable 
-            string myName = "Joseph";
-            
-            //string variable contatination
-            string message = "My name is " + myName;
-
-            //message.ToUpper(); converts message to uppercase
-            string capsMessage = message.ToUpper();
-
-            //message.ToLower(); converts the string message to lowercase
-            string lowerCaseMessage = message.ToLower();
-
-            
-            Console.WriteLine(lowerCaseMessage);
+            //Changing the color properties of the console
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Hello Joe");
+            Console.WriteLine("C# is cool!");
             Console.Read();
         }
     }
