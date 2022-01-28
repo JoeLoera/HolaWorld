98d2ab0e4198923ba19c1d9d48e2e165c5d04505
Joseph Loera <joeloera7@gmail.com>
2022-01-27 20:54:04 -0800

    Using Int variables to add

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,9 +7,20 @@ namespace HolaWorld
         //Entry point of our program
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola World!");
-            Console.WriteLine("Hello Joseph!");
-            Console.Read();
+            //declaring a variable
+            int num1;
+            //assigning a value to a variable
+            num1 = 13;
+
+            //declaring and initializing a variable in one go
+            int num2 = 23;
+
+            //sum will be used to add the sum of the two integer variables
+            int sum = num1 + num2;
+
+            Console.WriteLine(sum);
+
+
         }
     }
 }
