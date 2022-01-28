9eb849779941c61f34beccc31740109d4b45f543
Joseph Loera <joeloera7@gmail.com>
2022-01-27 20:43:45 -0800

    Add project files.

---
+++ HolaWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HolaWorld
+{
+    internal class Program
+    {
+        //Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola World!");
+            Console.WriteLine("Hello Joseph!");
+            Console.Read();
+        }
+    }
+}
