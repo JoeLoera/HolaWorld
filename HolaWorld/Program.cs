965566d7faedc955f9b5351254b352398ec58676
Joseph Loera <joeloera7@gmail.com>
2022-02-03 23:48:36 -0800

    Starting to write datatypes and values Printing each one in the console

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,31 +7,14 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            //ask the user to enter a string
-            Console.WriteLine("Enter a string here: ");
-            //read the input form the user
-            string input = Console.ReadLine();
-            //Ask the user to select a character to search for
-            Console.WriteLine("Select a character to search: ");
-            //Read the character input to search
-            char searchInput = Console.ReadLine()[0];
-            //Gets the index of the character from the string.
-            int searchIndex = input.IndexOf(searchInput);
-            //Print the Index as a search result on console.
-            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
-
-            Console.WriteLine("Enter first name: ");
-            //read the first name
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter last name: ");
-            //Read the last name
-            string lastName = Console.ReadLine();
-
-            //Concatinate the firstName and lastName variable and assign that to fullName variable.
-            string fullName = string.Concat(firstName, " ", lastName);
-            //Prints the full name on the console.
-            Console.WriteLine("Your full name is {0}", fullName);
-            Console.ReadKey();
+            //Print a byte value. Ranges between 0 to 255
+            byte myByte = 25;
+            Console.WriteLine(myByte);
+            //Print an sbyte value. Ranges between -128 to 127
+            sbyte mySByte = -25;
+            Console.WriteLine(mySByte);
+            //Print an int value. Ranges between -2,147,483,648 to -2,147,483,647
+             
         }
     }
 }
