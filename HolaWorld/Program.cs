f82c7b4ae7e89ca280baa7be4826c5afa94fd1e2
Joseph Loera <joeloera7@gmail.com>
2022-02-03 21:36:04 -0800

    All of these string manipulations produce the desired result although different than the course solution. Except Substring. Still working out that solution

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,33 +7,24 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the string here: ");
-            //Read first input string
-            string input = Console.ReadLine();
+            //declare a string variable with no value
+            string grogu;
+            //Print on the console "Please eneter your name and press enter"
+            Console.WriteLine("Please enter your name and press enter: ");
+            //assign the entered string to variable grogu
+            grogu = Console.ReadLine();
+            //Print the new entered value of grogu
+            Console.WriteLine(grogu);
+            //Converts the grogu string to uppercase
+            Console.WriteLine(grogu.ToUpper());
+            //Convert the grogu string to lowercase
+            Console.WriteLine(grogu.ToLower());
+            //Trim the white space before and after the string
+            Console.WriteLine(grogu.Trim());
+            //Still having an issue with the substring
+            string groguSubString = String.Format("Substring value : {0}", groguSubString.Substring) (0,5);
 
-            Console.Write("Enter a Character to search: ");
-            //Read the character input to search.
-            char searchInput = Console.ReadLine()[0];
-            //Gets the Index of the character from the string
-            int searchIndex = input.IndexOf(searchInput);
-            //Prints the Indez as a search result on console.
-            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
 
-            //Asks the user to enter their first name
-            Console.WriteLine("Enter first name: ");
-            //Read the first name
-            string firstName = Console.ReadLine();
-            //Asks the user to enter their first name
-            Console.Write("Enter last name: ");
-            //Read the last name
-            string lastName = Console.ReadLine();
-
-            //Concatinate the firstName and lastName variable and assign that to fullName variable.
-            string fullName = string.Concat(firstName, " ", lastName);
-            //Prints the full name on the console.
-            Console.WriteLine("Your full name is {0}", fullName);
-
-            Console.ReadKey();
         }
     }
 }
