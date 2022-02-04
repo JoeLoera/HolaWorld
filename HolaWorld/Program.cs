5dd01f7412a24c47f6a377eee554d82d945f6220
Joseph Loera <joeloera7@gmail.com>
2022-02-03 21:12:23 -0800

    String methods part 2 Index search a users response Concat string

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,26 +7,31 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            //declare a string variable with no value
-            string myName;
-            //Request the user to enter their name
-            Console.WriteLine("Please enter your name and press enter");
-            //assign to the users input to the value of myName
-            myName = Console.ReadLine();
-            //Change the format of myName to uppercase and assign it to variable myNameUpper
-            string myNameUpperCase = String.Format("Upper case: {0}", myName.ToUpper());
-            //assign the new uppercase value of the string to myNameUpperCase
-            Console.WriteLine(myNameUpperCase);
-            //Change the format of myName to lower case and assign it to variable myNameUpper
-            string myNameLowerCase = String.Format("Lower case: {0}", myName.ToLower());
-            //assign the new uppercase value of the string to myNameLowerCase
-            Console.WriteLine(myNameLowerCase);
-            //trim empty spaces at the beginning and end of the string
-            string myNameTrimmed = String.Format("Trimmed value : {0}", myName.Trim());
-            Console.WriteLine(myNameTrimmed);
-            //substring provides a specific part of the string
-            string myNameSubString = String.Format("Substring value : {0}", myName.Substring(1, 2));
-            Console.WriteLine(myNameSubString);
+            Console.Write("Enter the string here: ");
+            //Read first input string
+            string input = Console.ReadLine();
+
+            Console.Write("Enter a Character to search: ");
+            //Read the character input to search.
+            char searchInput = Console.ReadLine()[0];
+            //Gets the Index of the character from the string
+            int searchIndex = input.IndexOf(searchInput);
+            //Prints the Indez as a search result on console.
+            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
+
+            //Asks the user to enter their first name
+            Console.WriteLine("Enter first name: ");
+            //Read the first name
+            string firstName = Console.ReadLine();
+            //Asks the user to enter their first name
+            Console.Write("Enter last name: ");
+            //Read the last name
+            string lastName = Console.ReadLine();
+
+            //Concatinate the firstName and lastName variable and assign that to fullName variable.
+            string fullName = string.Concat(firstName, " ", lastName);
+            //Prints the full name on the console.
+            Console.WriteLine("Your full name is {0}", fullName);
 
             Console.ReadKey();
         }
