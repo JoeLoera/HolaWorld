bf860e485de82d75079693c1c400b02009726e78
Joseph Loera <joeloera7@gmail.com>
2022-02-06 00:03:18 -0800

    Creating a method, making the method static like our main and calling the method in our main method

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -3,24 +3,20 @@
 namespace HolaWorld
 {
 
-    internal class Program
+    class Program
     {
-        //Constants are immutable values which are known
-        //at compile time and do not change for the life of the program
-
-        const double PI = 3.1415926;
-        const int weeks = 52, months = 12;
-        const int days = 365;
+ 
         static void Main(string[] args)
         {
-            //fields are any variable that are within the class without the method
-            //Sjhadkfolasdlhkaslasdlaksldhfas
+            //WriteSomething(); method cannot be called from out main method because our main method is static. WriteSomething(); method must also be static
+            WriteSomething();
 
-            Console.WriteLine(days);
-            Console.WriteLine(weeks);
-            Console.WriteLine(months);
-
-
+        }
+        //access modifier (static) return type method name (parameter, 1 parameter 2 )
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+            Console.Read();
         }
     }
 }
