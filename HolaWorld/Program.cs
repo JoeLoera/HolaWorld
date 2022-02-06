afd931fd123e461c13d2d5dd1e8b8ad4c8e0abf1
Joseph Loera <joeloera7@gmail.com>
2022-02-06 00:12:53 -0800

    Working with void methods, WriteSomethingSpecific method requires arguments as opposed to WriteSomething method.

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -10,13 +10,17 @@ namespace HolaWorld
         {
             //WriteSomething(); method cannot be called from out main method because our main method is static. WriteSomething(); method must also be static
             WriteSomething();
-
+            WriteSomethingSpecific("I am an argument");
         }
         //access modifier (static) return type method name (parameter, 1 parameter 2 )
         public static void WriteSomething()
         {
             Console.WriteLine("I am called from a method");
-            Console.Read();
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
