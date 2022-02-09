51d4635115b5514878d735e7c8eae7680a49647d
Joseph Loera <joeloera7@gmail.com>
2022-02-08 21:44:59 -0800

    Examples of common and basic opperators

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -8,44 +8,74 @@ namespace HolaWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number!");
-            string userInput = Console.ReadLine();
-
             int num1 = 5;
-            int num2 = 0;
+            int num2 = 3;
+            int num3;
+
+            //unary operators
+            num3 = -num1;
+            Console.WriteLine("num3 is {0}", num3);
+
+            bool isSunny = true;
+            Console.WriteLine("Is it sunny today? {0}", !isSunny);
+
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            //increment that takes effect in the next line
+            Console.WriteLine("num is {0}", num++);
+            //pre increment that happens just before
+            Console.WriteLine("num is {0}", ++num);
+
+            //decrement operator
+            num--;
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num--);
+            //predecrement
+            Console.WriteLine("num is {0}", --num);
+
             int result;
 
-            try
-            {
-                result = num1 / num2;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Cant divide by zero!");
-            }
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num 2 is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num 2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num 2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num 2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num 2 is {0}", result);
 
-            try
-            {
-                int userInputAsInt = int.Parse(userInput);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Format exception, please enter the correct type next time.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Overflow exception, the number was too long or short for an int32");
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("ArgumentNullException, the value was empty(null)");
-            }
-            finally
-            {
-                Console.WriteLine("This is called anyways!");
-            }
+            //relational and type operators
+
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine("Result of num1 < num2 {0}", isLower);
+
+            //equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("Result of num1 == num2 {0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("Result of num1 != num {0}", isEqual);
+
+            //conditional operators
+            bool isLowerandSunny;
+            isLowerandSunny = isLower && isSunny;
+            //is only true if both conditions are true
+            Console.WriteLine("result of isLower && isSunny is {0}", isEqual);
+
+            //condition 1 or 2 
+            isLowerandSunny = isLower || isSunny;
+            //Console reflects true if either of the two conditions is true
+            // || is for or
+            Console.WriteLine("result of isLower || isSunny is {0}", isLowerandSunny);
 
             Console.ReadKey();
+
         } 
     }
 }
