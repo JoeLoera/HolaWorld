b6d45a7239c71cb3d462107afe75056d785d3f72
Joseph Loera <joeloera7@gmail.com>
2022-02-13 00:49:19 -0800

    First time using a Switch statement

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -4,46 +4,21 @@ namespace HolaWorld
 {
 
     class Program
-    {
-        static string username;
-        static string password;
-
+    { 
         static void Main(string[] args)
         {
-            Register();
-            Login();
-            Console.Read();
-        }
-
-        public static void Register()
-        {
-            Console.WriteLine("Please eneter your username");
-            username = Console.ReadLine();
-            Console.WriteLine("Please eneter your password");
-            password = Console.ReadLine();
-            Console.WriteLine("Registration completed");
-            Console.WriteLine("----------------------------------");
-        }
-
-        public static void Login()
-        {
-            Console.WriteLine("Please eneter your username");
-            if (username == Console.ReadLine())
+            int age = 19;
+            switch (age) 
             {
-                Console.WriteLine("Please eneter your password");
-                if (password == Console.ReadLine())
-                {
-                    Console.WriteLine("login successful");
-                }
-                else
-                {
-                    Console.WriteLine("login failed, wrong password. Restart program");
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("Login failed, wrong username. Restart program");
+                case 15:
+                    Console.WriteLine("Too young to party in the club");
+                    break;
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you then?");
+                    break;
             }
         }
     }
