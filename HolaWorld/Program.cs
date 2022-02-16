fd3e71f9c68cde8f3a3e71e06faa08a268feaba1
Joseph Loera <joeloera7@gmail.com>
2022-02-16 13:04:59 -0800

    Loops Challenge, creating a Loop that calculated the average score of students on a test

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,14 +7,38 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            for (int counter = 0; counter < 10; counter++)
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            while (input != "-1")
             {
-                if (counter % 2 == 0)
+                Console.WriteLine("last number was {0}", currentNumber);
+                Console.WriteLine("Please eneter the next score");
+                Console.WriteLine("Current amount of enteries {0}", count);
+                Console.WriteLine("Please enter -1 once you are ready to calculate the average");
+
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
                 {
-                    Console.WriteLine("Now comes an odd number!");
+                    Console.WriteLine("-----------------------------------------");
+                    double average = (double)total / (double)count;
+                    Console.WriteLine("The average score of your students {0}", average);
+                }
+                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20!");
+                    }
                     continue;
                 }
-                Console.WriteLine(counter);
+                count++;
             }
             Console.ReadKey();
         }
