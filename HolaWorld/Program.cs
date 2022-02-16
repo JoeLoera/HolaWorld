c6e7faea582611fadea9cb7f9b7a0eeea2c381db
Joseph Loera <joeloera7@gmail.com>
2022-02-15 17:59:41 -0800

    Intro to For Loops

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,41 +7,12 @@ namespace HolaWorld
     {
        static void Main(string[] args)
         {
-            int inputTemperature = 0;
-            string temperatureMessage = string.Empty;
-            string inputValue = string.Empty;
-
-            //takes input from console
-            Console.WriteLine("Enter the current temperature : ");
-            inputValue = Console.ReadLine();
-
-            //validate the input as valid input integer value
-            bool validInteger = int.TryParse(inputValue, out inputTemperature);
-
-            if (validInteger)
-            {
-                //if is valid integer then it will check for the conditions 
-                temperatureMessage = inputTemperature <= 15 ?
-                    //true
-                    "it is too cold here" :
-                    //false
-                    (inputTemperature >= 16 && inputTemperature <= 28) ? 
-                    //true
-                    "it is cold here" : 
-                    //false
-                    inputTemperature > 28 ?
-                    //true
-                    "it is hot here" : 
-                    //false
-                    "";
-                Console.WriteLine(temperatureMessage);
-            }
-            else
+            for (int counter = 0; counter < 50; counter+=5)
             { 
-                //in case if the input value is not a valid temperature.
-                Console.WriteLine("Not a valid temperature");
+                Console.WriteLine(counter);
             }
-            Console.ReadKey();
+            Console.WriteLine("For loop is done");
+            Console.Read();
         }
     }
 }
