5a5cb893d8d8aad68436e1bdf998bf2713232243
Joseph Loera <joeloera7@gmail.com>
2022-02-16 06:54:35 -0800

    Using break and continue in For loops

--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -5,19 +5,18 @@ namespace HolaWorld
 
     class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
-           int counter = 0;
-           string enteredText = "";
-           while (enteredText.Equals(""))
+            for (int counter = 0; counter < 10; counter++)
             {
-                Console.WriteLine("Please press eneter to increase the amount by one and anything else" + " + enter if you want to finish counting");
-                enteredText = Console.ReadLine();
-                counter++;
-                Console.WriteLine("Currrent people count is {0}", counter);
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine("Now comes an odd number!");
+                    continue;
+                }
+                Console.WriteLine(counter);
             }
-            Console.WriteLine("{0} people are inside the bus. Press Enter to close the program", counter);
-            Console.Read();
+            Console.ReadKey();
         }
     }
 }
