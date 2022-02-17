0bcfdd3f1d777a7e0dcc57aecba80d88d6d5663d
Joseph Loera <joeloera7@gmail.com>
2022-02-17 06:04:51 -0800

    Creating my first class

---
+++ HolaWorld/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaWorld
+{
+    //this class is a blueprint for a datatype
+
+    //create a public variable lastName of type string
+    //change the IntroduceMyself method, so it tells the whole name
+    //create two objects with full information about themself - firstName and lastName
+
+    internal class Human
+    {
+        //member variable
+        public string firstName;
+        public string lastName;
+
+        //member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,39 +7,16 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            string input = "0";
-            int count = 0;
-            int total = 0;
-            int currentNumber = 0;
+            Human sissy = new Human();
+            sissy.firstName = "Sissy";
+            sissy.lastName = "Wagner";
+            sissy.IntroduceMyself();
 
-            while (input != "-1")
-            {
-                Console.WriteLine("last number was {0}", currentNumber);
-                Console.WriteLine("Please eneter the next score");
-                Console.WriteLine("Current amount of enteries {0}", count);
-                Console.WriteLine("Please enter -1 once you are ready to calculate the average");
+            Human franz1 = new Human();
+            franz1.firstName = "Franzil";
+            franz1.lastName = "Ryan";
+            franz1.IntroduceMyself();
 
-                input = Console.ReadLine();
-                if (input.Equals("-1"))
-                {
-                    Console.WriteLine("-----------------------------------------");
-                    double average = (double)total / (double)count;
-                    Console.WriteLine("The average score of your students {0}", average);
-                }
-                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
-                {
-                    total += currentNumber;
-                }
-                else
-                {
-                    if (!(input.Equals("-1")))
-                    {
-                        Console.WriteLine("Please enter a value between 1 and 20!");
-                    }
-                    continue;
-                }
-                count++;
-            }
             Console.ReadKey();
         }
     }
