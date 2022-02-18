450608a7ba424a04fe19ffd430db6c254cab9133
Joseph Loera <joeloera7@gmail.com>
2022-02-17 21:09:27 -0800

    Practice with Properties, Setters and Getters pt 1

---
+++ HolaWorld/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaWorld
+{
+    class Box
+    {
+        //member variable
+        private int length = 3;
+        public int height;
+        public int width;
+        public int volume;
+
+        public void SetLength(int length)
+        {
+            if (length < 0)
+            {
+                throw new Exception("Length should be higher than 0");
+            }
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int GetVolume()
+        { 
+            return this.length * this.height * this.width;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}", 
+                length, height, width, volume = length*height*width);
+        }
+    }
+}
--- HolaWorld/Program.cs
+++ HolaWorld/Program.cs
@@ -7,26 +7,14 @@ namespace HolaWorld
     {
         static void Main(string[] args)
         {
-            Human joe = new Human("Joe","Loera","brown", 29);
-            joe.IntroduceMyself();
-
-            Human julie = new Human("Julie", "Delgadillo", "purple", 29);
-            julie.IntroduceMyself();
-
-            Human basicHuman = new Human();
-            basicHuman.IntroduceMyself();
-
-            Human natalie = new Human("Natalie");
-            natalie.IntroduceMyself();
-
-            Human jacob = new Human("Jacob", "Jacob");
-            jacob.IntroduceMyself();
-
-            Human richard = new Human("Richard", "Scott", 32);
-            richard.IntroduceMyself();
-
-
-            Console.ReadKey();
+            //creating an instance of Box
+            Box box = new Box();
+            //box.length = 3;
+            box.SetLength(-4);
+            box.height = 4;
+            box.width = 5;
+            Console.WriteLine("Box volume is " + box.GetVolume());
+            box.DisplayInfo();
         }
     }
 }
